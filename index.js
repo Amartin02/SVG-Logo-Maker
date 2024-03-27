1740eef1ed005509de704b35c1ffdd4e0bf8ed89
Amartin02 <Tankfurnace641@gmail.com>
2024-03-27 16:04:03 -0600

    added proper inquirer and finished function with svg file being made. still needs to be bigger and centered then tests

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const jest = require("jest");
 const colors = require("colors");
-
+const fs = require("fs");
 //prompt for command line
 inquirer
   .prompt([
@@ -32,17 +32,23 @@ inquirer
   ])
   //.then to create the logo after prompts are asked
   .then((Response) =>
-    fs.writeFile("logo.svg", Response, (err) => {
+    fs.writeFile("logo.svg", createLogo(Response), (err) => {
       err ? console.log(err) : console.log("Generated logo.svg");
     })
   );
-//function for the prompt that asks about the shape
-function shape(logoShape) {
-  if (logoShape === "Square") {
-    return "";
-  } else if (logoShape === "Circle") {
-    return "";
-  } else if (logoShape === "Triangle") {
-    return "";
+//function for the prompt that uses the inputs to create the desired logo
+function createLogo(userChoices) {
+  if (userChoices.logoShape === "Square") {
+    return `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <rect width="150" height="150" x="10" y="10" style="fill:${userChoices.logoColor};" />
+      Sorry, your browser does not support inline SVG.  
+      <text x="75" y="90" fill="${userChoices.textColor}">${userChoices.logoText}</text></svg>`;
+  } else if (userChoices.logoShape === "Circle") {
+    return `<svg height="300" width="200" xmlns="http://www.w3.org/2000/svg">
+    <circle r="45" cx="50" cy="50" fill="${userChoices.logoColor}" />
+    Sorry, your browser does not support inline SVG.  
+    <text x="40" y="55" fill="${userChoices.textColor}">${userChoices.logoText}</text></svg> `;
+  } else if (userChoices.logoShape === "Triangle") {
+    return `<svg height="300" width="200"><polygon points="75,75 25,75 50,25" fill="${userChoices.logoColor}"  transform="  scale(1)"></polygon><text><text x="41" y="55" fill="${userChoices.textColor}">${userChoices.logoText}</text></svg>`;
   }
 }
